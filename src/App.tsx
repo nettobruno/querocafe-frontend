4793d7babf254aaa66f77407ed622002c869061e
Bruno Netto <brunonettomac@outlook.com>
2022-08-05 18:30:51 -0300

    Configuração básica do tailwindcss e postcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 function App() {
   return (
-    <h1>Quero café</h1>
+    <h1 className="text-3xl font-bold text-amber-900">Quero café</h1>
   )
 }
 
---
+++ tailwind.config.cjs
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.tsx'
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        
+      },
+      fontFamily: {
+        
+      },
+      colors: {
+        
+      },
+    },
+  },
+  plugins: [],
+}
